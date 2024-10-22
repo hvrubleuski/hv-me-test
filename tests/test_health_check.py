aa7e109396e5c30d91d542001094a1d1a0935121
Henadzi Vrubleuski <hvrubleuski>
2024-10-22 17:06:09 +0300

    Added basic logic for handling exceptions

--- tests/test_health_check.py
+++ tests/test_health_check.py
@@ -7,11 +7,14 @@ def test_app_health_check():
     url = os.getenv('HV_ME_WEBSITE_URL_PROD')
     if not url:
         pytest.fail("HV_ME_WEBSITE_URL_PROD environment variable is not set")
-    response = requests.get(url, timeout=10)
-    ## Verify response code is 200
-    assert 200 == response.status_code
-    content = str(response.content)
-    ## Verify the page contains correct welcome message
-    assert 'Hello, This Page is Under Construction' in content
-    ## verify there is single <h2> element on the page
-    assert 1 == content.count('<h2>')
+    try:
+        response = requests.get(url, timeout=10)
+        ## Verify response code is 200
+        assert 200 == response.status_code
+        content = str(response.content)
+        ## Verify the page contains correct welcome message
+        assert 'Hello, This Page is Under Construction' in content
+        # ## verify there is single <h2> element on the page
+        assert 1 == content.count('<h2>')
+    except requests.exceptions.RequestException as e:
+        pytest.fail(f"Request to {url} failed with exception: {e}")
